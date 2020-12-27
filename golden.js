6e033310cd218e179643353253af55730cec23b4
rahaf97ss <rahaf.saeed1126@gmail.com>
2020-12-27 16:33:07 +0200

    added java page

---
+++ golden.js
@@ -0,0 +1,15 @@
+var meals= confirm (' if you click ok you are male and will loop 4 times, and if you click cancel you are female and will loop 2 times');
+if (meals){
+    for (var x=0; x<=3; x++){
+document.write("<p> Burger </p> ")}
+    }
+ 
+
+else{
+
+for( var x=0; x<2; x++){
+document.write("<p> Burger </p>")}
+}
+
+
+
